0ca50f7faf467a97b13087ec6b12f868604c1c53
yektaty <yektayt10@gmail.com>
2025-04-02 02:05:52 +0330

    no masege

--- main.cpp
+++ main.cpp
@@ -1,31 +1,45 @@
 #include "Student.h"
+#include "Cours.h"
 #include "Professor.h"
 #include "Admin.h"
 
 int main()
 {
+    Cours*headcourse=nullptr;
+    Student*headstd=nullptr;
+    Professor*headprof=nullptr;
 
     cout << MAGENTA << "✨ Welcome to the Yekestan program ✨" << RESET << endl;
     cout << endl;
-    cout << GREEN << "Please select one of the items" << endl;
-    cout << "1) I'm admin\n2)I have already registered\n3)I want to register now" << RESET << endl;
-    int num;
-    cin>>num;
-    switch (num)
+    while (1)
     {
-    case 1:
-        //بره بخش ادمین و پیده سازی اون
-        break;
-    case 2:
-        //بخش اینه اصلا استاده یا دانشجو و بعد وارد بخش کاربری
-        break;
-    case 3:
-        //دوباره بپرسه که دانشجو هست یا ن وو ادامه بده به زندگانیش
-        break;
-    default:
-    
-        break;
+        cout << GREEN << "Please select one of the items.For example, if you are a student and want to register, enter number 35 or if you are a professor and have already registered, enter number 24, if you are also an admin, enter only the number 1 and end with the number 0" << endl;
+        cout << "1)I'm Admin\n2)I'm Professor\n3)I'm Student\n4)I have already registered\n5)I want to register now" << RESET << endl;
+        int num;
+        cin >> num;
+        switch (num)
+        {
+        case 1:
+            Admin_page(headcourse,headstd,headprof);
+            break;
+        case 24:
+            // بخش اینه اصلا استاده یا دانشجو و بعد وارد بخش کاربری
+            break;
+        case 25:
+            // دوباره بپرسه که دانشجو هست یا ن وو ادامه بده به زندگانیش
+            break;
+        case 34:
+            // دوباره بپرسه که دانشجو هست یا ن وو ادامه بده به زندگانیش
+            break;
+        case 35:
+            // دوباره بپرسه که دانشجو هست یا ن وو ادامه بده به زندگانیش
+            break;
+        default:
+            cerr << RED << "The number entered is incorrectPlease try again." << RESET << endl;
+            break;
+        case 0:
+            return;
+        }
     }
-
     return 0;
 }
